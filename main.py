6657f4b3241c975db8544354ed8b96cba5417a5a
WeAdjo <pashok_spider@mail.ru>
2020-04-08 16:12:14 +0300

    Our first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+user = "Павел"
+url = 'https://www.avito.ru/sankt_peterburg_i_lo'
+action = "покупка"
+
+message = 'Пользователь {} зашел на сайт {} и выполнил действие: {}'.format(user, url, action)
+
+print(message)
+# Пользователь Павел зашел на сайт и выполнил действие: покупка
